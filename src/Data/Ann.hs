cb8ce250568ec271d6ad0fdffd0ee20dd5c9623d
Arnaud Spiwack <arnaud@spiwack.net>
2022-05-12 20:41:06 +0200

    Basic interface

--- src/Data/Ann.hs
+++ src/Data/Ann.hs
@@ -1,5 +1,35 @@
+{-# LANGUAGE DerivingVia #-}
+
 module Data.Ann
-    ( Ann,
-    ) where
+  ( Ann,
+    project,
+    extract,
+    unsafeExtract,
+  )
+where
+
+import Data.Functor.Identity
 
 newtype Ann a = Squash a
+  deriving
+    (Functor, Applicative, Monad)
+    via Identity
+  deriving
+    (Semigroup, Monoid, Read, Show)
+    via (Identity a)
+
+instance Eq (Ann a) where
+  _ == _ = True
+
+instance Ord (Ann a) where
+  _ <= _ = True
+  compare _ _ = EQ
+
+unsafeExtract :: Ann a -> a
+unsafeExtract (Squash a) = a
+
+extract :: Ann a -> IO a
+extract = return . unsafeExtract
+
+project :: a -> Ann a
+project = Squash
